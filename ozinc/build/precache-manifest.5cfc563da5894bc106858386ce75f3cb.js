7678b286d15655cb577e1f0af2e16b6ae9388f7e
Plutone11011 <borelliplutone@gmail.com>
2019-12-12 16:51:18 +0100

    commit for production build

---
+++ ozinc/build/precache-manifest.5cfc563da5894bc106858386ce75f3cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01431f391032f38e4ac69064a64556f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07752351bc419af3e0f5",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "68e8da27f23e87d38284",
+    "url": "/static/css/main.5aad9dc8.chunk.css"
+  },
+  {
+    "revision": "07752351bc419af3e0f5",
+    "url": "/static/js/2.60d85388.chunk.js"
+  },
+  {
+    "revision": "68e8da27f23e87d38284",
+    "url": "/static/js/main.e1be9d15.chunk.js"
+  },
+  {
+    "revision": "2cdea586dcd38b5f8297",
+    "url": "/static/js/runtime-main.3988c4d3.js"
+  }
+]);

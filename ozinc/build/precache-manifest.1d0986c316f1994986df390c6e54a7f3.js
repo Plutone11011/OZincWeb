f4ac103b37801445484f5af42f89686db355a312
Plutone11011 <borelliplutone@gmail.com>
2019-12-28 18:42:44 +0100

    changed minizinc distance output

---
+++ ozinc/build/precache-manifest.1d0986c316f1994986df390c6e54a7f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0424a21beedaf19f38a5ca2bb8d50a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2666f529fea36625502",
+    "url": "/static/css/2.fa49a92f.chunk.css"
+  },
+  {
+    "revision": "f651f84db2bc376bb2f9",
+    "url": "/static/css/main.5aad9dc8.chunk.css"
+  },
+  {
+    "revision": "e2666f529fea36625502",
+    "url": "/static/js/2.ee1ac1eb.chunk.js"
+  },
+  {
+    "revision": "f651f84db2bc376bb2f9",
+    "url": "/static/js/main.32345b16.chunk.js"
+  },
+  {
+    "revision": "2cdea586dcd38b5f8297",
+    "url": "/static/js/runtime-main.3988c4d3.js"
+  }
+]);
